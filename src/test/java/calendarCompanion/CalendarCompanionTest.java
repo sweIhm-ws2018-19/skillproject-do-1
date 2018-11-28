54f76f333fad5965bfe106e943c0e72c8e3f9bcf
JohnsoNi <JohnsoNi@PC02975.intra.ads-root.de>
2018-11-28 17:01:58 +0100

    more test classes added but still need to be coded

---
+++ src/test/java/calendarCompanion/CalendarCompanionTest.java
@@ -0,0 +1,30 @@
+package calendarCompanion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import calendarCompanion.model.ToDoList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalendarCompanionTest {
+	
+	List<String> list = new ArrayList<String>();
+	
+	    @Test
+	    public void setToDoList() {
+	    	
+	    	ToDoList toDoList = new ToDoList(list);
+	    	
+	    	
+
+	     
+	    }
+
+
+}
