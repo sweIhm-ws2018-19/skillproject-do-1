1679bc5e58700b21b009df0fd6c91ef618f71daa
JohnsoNi <JohnsoNi@PC02975.intra.ads-root.de>
2018-11-28 16:04:30 +0100

    added test package

---
+++ src/test/java/calendarCompanion/testTest.java
@@ -0,0 +1,14 @@
+package calendarCompanion;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
