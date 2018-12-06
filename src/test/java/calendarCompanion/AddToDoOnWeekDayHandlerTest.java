f06bcf717166045e199ca23d474c2995406c35d3
Nicole Johnson <Nicole.Johnson@vsa.de>
2018-12-06 12:46:19 +0100

    included canHandle test in addtodo handler

--- src/test/java/calendarCompanion/AddToDoOnWeekDayHandlerTest.java
+++ src/test/java/calendarCompanion/AddToDoOnWeekDayHandlerTest.java
@@ -2,10 +2,29 @@ package calendarCompanion;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import calendarCompanion.handlers.AddToDoOnWeekDayHandler;
 
 public class AddToDoOnWeekDayHandlerTest {
-
+    private AddToDoOnWeekDayHandler addToDoOnWeekDayHandler;
+    
+    @Before
+    public void setup() {
+        addToDoOnWeekDayHandler = new AddToDoOnWeekDayHandler();
+    }
+    @Test
+    public void testCanHandle() {
+        final HandlerInput inputMock = Mockito.mock(HandlerInput.class);
+        when(inputMock.matches(any())).thenReturn(true);
+        assertTrue(addToDoOnWeekDayHandler.canHandle(inputMock));
+    }
 
 
 }
