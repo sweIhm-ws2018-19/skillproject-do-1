3c13fd4aee9645eeb0f1015642a46cea76b7a51f
Nicole Johnson <Nicole.Johnson@vsa.de>
2019-01-05 13:49:42 +0100

    measurement improvements

--- src/main/java/calendarCompanion/HttpRequests/DateTimeDE.java
+++ src/main/java/calendarCompanion/HttpRequests/DateTimeDE.java
@@ -2,7 +2,6 @@ package calendarCompanion.HttpRequests;
 
 import calendarCompanion.model.DateAndTime;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
